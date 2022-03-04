d83b6e983d48b989d348c9051ac93e8b600bbb58
AlexeevaZhanna <shannita@rambler.ru>
2022-03-04 03:15:00 +0300

    Shop

--- src/com/company/FileKassirApp.java
+++ src/com/company/FileKassirApp.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class FileKassirApp {
 
 
-    public static void main(String[] args)  {
+   public static void main(String[] args)  {
 
         new File("Kassir.txt");
         System.out.println("Ввести данные или отобразить?");
@@ -14,24 +14,33 @@ public class FileKassirApp {
         System.out.println("2. Отобразить");
 
         Scanner sc = new Scanner(System.in);
-        int option = sc.nextInt();
+       int option = sc.nextInt();
 
-        if (option == 1) {
-            System.out.println("Заполнить данные и сохранить");
-            dbWrite(sc);
-        } else if (option == 2) {
-            System.out.println("Список кассиров:");
-            dbRead();
-        }
+         try {                                                // обработка(try-catch)
+             if (option == 1) {
+                 System.out.println("Заполнить данные и сохранить");
+                 dbWrite(sc);
+             } else if (option == 2) {
+                 System.out.println("Список кассиров:");
+                 dbRead();
+             }
+            else if (optionNumber(option)) {
+              System.out.println("Попробуйте еще раз");
+             }
+         } catch (Exception e) {
+             System.out.println(e.getMessage());
+         } finally {
+             System.out.println("Завершено");
+         }
     }
 
     private static void dbWrite(Scanner sc) {    // запись объекта в файл Kassir.txt
         try (
-                FileWriter fw = new FileWriter("Kassir.txt" , true)) {
-            fw.write("name:"+sc.next() + " ");
-            fw.write("age:"+sc.next() + " ");
-            fw.write("number:"+sc.next() + " ");
-            fw.write("sale:"+sc.next() + " ");
+                FileWriter fw = new FileWriter("Kassir.txt", true)) {
+            fw.write("name:" + sc.next() + " ");
+            fw.write("age:" + sc.next() + " ");
+            fw.write("number:" + sc.next() + " ");
+            fw.write("sale:" + sc.next() + " ");
             fw.write("\n");
             fw.flush();
             System.out.println("Информация записана в файл");
@@ -39,18 +48,28 @@ public class FileKassirApp {
             e.printStackTrace();
         }
 
-        }
+    }
 
-   private static void dbRead () {  //чтение из файла Kassir.txt
+    private static void dbRead() {  //чтение из файла Kassir.txt
 
-       try (BufferedReader br = new BufferedReader(new FileReader("Kassir.txt"))) {
-           String text;
-           while ((text = br.readLine()) != null)  {
+        try (BufferedReader br = new BufferedReader(new FileReader("Kassir.txt"))) {
+            String text;
+            while ((text = br.readLine()) != null) {
 
-               System.out.println(text);
-           }}
-         catch (IOException e){
-               e.printStackTrace();
-           }
+                System.out.println(text);
             }
-}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean optionNumber(int option) throws Exception {   // throws Exception
+               if (option != 1 & option != 2) {
+                throw new Exception("Получено неверное значение. Попробуйте еще раз");
+           }
+        return false;
+               }
+    }
+
+
+
